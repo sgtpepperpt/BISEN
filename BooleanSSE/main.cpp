e2ed2807253fd4ff623fb3b8e88472a73426a0c7
Bernardo Ferreira <bernardolferreira@gmail.com>
2016-11-16 10:02:59 +0000

    Initial Commit

---
+++ BooleanSSE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BooleanSSE
+//
+//  Created by Bernardo Ferreira on 16/11/16.
+//  Copyright © 2016 Bernardo Ferreira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

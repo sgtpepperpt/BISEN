4fa2de952d549eb5c6bd08adae41683f85762bd9
Guilherme Borges <g.borges@campus.fct.unl.pt>
2018-02-07 04:30:12 +0000

    more queries on script

--- results/searches.sh
+++ results/searches.sh
@@ -4,16 +4,16 @@ NUM_TESTS=1
 SIZES=(50000 100000 200000 517402)
 export NUM_QUERIES=4
 
-#declare -a QUERIES=("enron && time" "enron && time && inform && work && call" "enron && time && inform && work && call && discuss && meet && week && receiv && dai"
-#                    "enron || time" "enron || time || call || work || inform" "enron || time || inform || work || call || discuss || meet || week || receiv || dai"
-#                    "(call || enron) && (time || attach)" "(call || enron) && (time || attach) && (inform || work) && (meet || week)"
-#                    "(call && enron) || (time && attach)" "(call && enron) || (time && attach) || (inform && work) || (meet && week)"
-#                    "!enron && !time" "!(enron && time)" "!enron || !time" "!(enron || time)"
-#                    )
-
-declare -a QUERIES=("enron && time && inform && work && call && discuss && meet && week && receiv && dai"
-                    "!(enron && time)" "!enron || !time" "!(enron || time)"
+declare -a QUERIES=("enron && time" "enron && time && inform && work && call" "enron && time && inform && work && call && discuss && meet && week && receiv && dai"
+                    "enron || time" "enron || time || call || work || inform" "enron || time || inform || work || call || discuss || meet || week || receiv || dai"
+                    "(call || enron) && (time || attach)" "(call || enron) && (time || attach) && (inform || work) && (meet || week)"
+                    "(call && enron) || (time && attach)" "(call && enron) || (time && attach) || (inform && work) || (meet && week)"
+                    "!enron && !time" "!(enron && time)" "!enron || !time" "!(enron || time)"
                     )
+
+#declare -a QUERIES=("enron && time && inform && work && call && discuss && meet && week && receiv && dai"
+#                    "!(enron && time)" "!enron || !time" "!(enron || time)"
+#                    )
 
 # initial compile
 (cd ../src/Test && make clean && make)
